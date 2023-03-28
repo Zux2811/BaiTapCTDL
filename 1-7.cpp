d623b8586dbb3e74b8127da37fd325f757b57561
Zux2811 <125178485+Zux2811@users.noreply.github.com>
2023-03-28 15:37:03 +0700

    Add files via upload

---
+++ 1-7.cpp
@@ -0,0 +1,30 @@
+#include<stdio.h>
+#include<conio.h>
+struct Ngay
+{
+    int Ngay, Thang, Nam;
+};
+typedef Ngay N;
+void nhapNgay(N &);
+void xuatNgay(N);
+void nhapNgay(N &n);
+{
+   printf("Nhap Ngay: \n");
+   scanf("%d", &n.Ngay);
+   printf("NhapThang: \n");
+   scanf("%d", &n.Thang);
+   printf("NhapNam: \n");
+   scanf("%d", &n.Nam);
+}
+void xuat(N.n)
+{
+    printf(%d%d%d, n.Ngay, n.Thang, n.Nam);
+}
+int main()
+{
+N n;
+nhapNgay(n);
+xuatNgay(n);
+getch()
+return 0;
+}
